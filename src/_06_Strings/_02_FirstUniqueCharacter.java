e2d78cecf34a9471b043710c1fd120219b110e1c
Arman Patel <vanixgaming634@gmail.com>
2026-01-18 00:30:08 +0530

    Strings medium

---
+++ src/_06_Strings/_02_FirstUniqueCharacter.java
@@ -0,0 +1,18 @@
+package _06_Strings;
+
+public class _02_FirstUniqueCharacter {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        for(char c:s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for(int i = 0 ;i <s.length(); i++) {
+            if(freq[s.charAt(i) -'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/_06_Strings/_03_LongestSubstringWithoutRepeatCharacter.java
@@ -0,0 +1,25 @@
+package _06_Strings;
+
+import java.util.Arrays;
+
+public class _03_LongestSubstringWithoutRepeatCharacter {
+    public int lengthOfLongestSubstring(String s) {
+        int[] lastSeen = new int[128];
+        Arrays.fill(lastSeen,-1);
+        int left=0;
+        int maxLen = 0;
+
+        for(int right = 0; right<s.length(); right++) {
+            char c = s.charAt(right);
+
+            if(lastSeen[c] >= left) {
+                left = lastSeen[c]+1;
+            }
+
+            lastSeen[c] = right;
+            maxLen = Math.max(maxLen,right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
