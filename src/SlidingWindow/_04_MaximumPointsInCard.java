2fe061fe212e46d6db0353a2ed1aef606ad5c4bd
Arman Patel <vanixgaming634@gmail.com>
2026-01-24 23:56:37 +0530

    fixed sliding-window

---
+++ src/SlidingWindow/_04_MaximumPointsInCard.java
@@ -0,0 +1,19 @@
+package SlidingWindow;
+class SolutionMaxPoints {
+    public int maxScore(int[] cardPoints, int k) {
+        int leftSum = 0, rightSum = 0;
+
+        for (int i  = 0; i<k-1;i++) {
+            leftSum += cardPoints[i];
+        }
+        int rightIndex = cardPoints.length-1;
+        for(int i = k-1; i>=0;i--){
+            leftSum -= cardPoints[i];
+            rightSum += cardPoints[rightIndex];
+            rightIndex--;
+        }
+        return Math.max(leftSum,rightSum);
+    }
+}
+public class _04_MaximumPointsInCard {
+}
